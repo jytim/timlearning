24b255f55cc1f0fb31386491a6ab202890c45629
timlearning <jytim0423@gmail.com>
2023-12-22 18:47:31 +0800

    123

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-$(index.html).ready(function(){
-    var openModalBtn = $("#openModalBtn");
-    var closeModalBtn = $("#closeModalBtn");
-    var modal = $("#myModal");
 
-    openModalBtn.click(function(){
-        modal.show();
-    });
+var openModalBtn = $("#openModalBtn");
+var closeModalBtn = $("#closeModalBtn");
+var modal = $("#myModal");
 
-    closeModalBtn.click(function(){
-        modal.hide();
-    });
-
-    $(window).click(function (event) {
-        if (event.target === modal[0]) {
-            modal.hide();
-        }
-    });
+openModalBtn.click(function(){
+    modal.show();
 });
+
+closeModalBtn.click(function(){
+    modal.hide();
+});
+
+$(window).click(function (event) {
+    if (event.target === modal[0]) {
+        modal.hide();
+    }
+});
+
 
 
 
